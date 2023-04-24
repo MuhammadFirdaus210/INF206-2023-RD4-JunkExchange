20c1483206913e1866de5e9f2df247d3ac16b736
Alfan Nufi <alfan.s.nufi@gmail.com>
2023-04-24 19:13:03 +0700

    feat: return redirect on register

--- JunkExchange/app/Http/Controllers/RegisterController.php
+++ JunkExchange/app/Http/Controllers/RegisterController.php
@@ -23,5 +23,6 @@ class RegisterController extends Controller
         $validatedData['password'] = Hash::make($validatedData['password']);
 
         User::create($validatedData);
+        return redirect('/login');
     }
 }
