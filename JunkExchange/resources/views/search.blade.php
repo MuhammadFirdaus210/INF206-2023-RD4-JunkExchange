2f3eb50d7f72065762e7b1075fab4aae322e14f9
Alfan Nufi <alfan.s.nufi@gmail.com>
2023-04-27 15:20:33 +0700

    chore: minor changes

--- JunkExchange/resources/views/search.blade.php
+++ JunkExchange/resources/views/search.blade.php
@@ -1,42 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/style.css">
-    
-    <title>EXP</title>
+@extends('layouts.main')
 
-</head>
-<body>
+@section('container')
 
-    
-<nav class="nav px-2 py-2 shadow" style="background-color: #2ABA86">
-    <ul class="nav">
-        <li class="nav-item">
-            <a class="nav-link fs-30 text-white" href="/">Me</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link fs-30 text-white" href="/about">Tentang</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link fs-30 text-white" href="/contact">Hubungi</a>
-        </li>
-    </ul>
-</nav>
-    <div class="container-contain">
-     <div class="toolbar">
-        <a href="#">Keranjang</a>
-        <a href="#">Kotak</a>
-        <a href="#">Vas Bunga</a>
-        <a href="#">Boneka</a>
-        <a href="#">Bunga</a>
-        <a href="#">Mainan</a>
-        <hr>
-    </div>
-
+@include('partials.hypernav')
         <div class="hero">
             <img src="images/exmple.jpg" class="rounded mx-auto d-block" style="width: 70%; height: 150px; border-radius: 5px; object-fit: cover;">
         </div>
@@ -48,9 +14,8 @@
             <p><a href="#">Banyak Dicari</a></p>
             <p><a href="#">Unik</a></p>
             <p><a href="#">Bermanfaat</a></p>
-  </div>
-</h6>
-</div>
+        </div>
+        </h6>
 
         <div class="row">
             <div class="col-6 col-sm-4">
@@ -132,6 +97,5 @@
                 </div>
             </div>
         </div>
-</body>
-<link rel="stylesheet" href="css/search.css">
-</html>
+
+@endsection
