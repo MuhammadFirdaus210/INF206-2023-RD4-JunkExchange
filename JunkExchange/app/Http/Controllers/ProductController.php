6b9972021774e01321b9031075cb2d56fe6678c3
MuhammadFirdaus210 <m_firdaus@mhs.unsyiah.ac.id>
2023-05-10 16:22:39 +0700

    feat : product Controller

--- JunkExchange/app/Http/Controllers/ProductController.php
+++ JunkExchange/app/Http/Controllers/ProductController.php
@@ -4,13 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Datas;
+use App\Models\User;
+use App\Notifications\NotifContri;
+use Illuminate\Support\Facades\Notification;
+
 
 class ProductController extends Controller
 {
-    public function index($id) 
-    {    
-        return view('product', [
-            "item" => Datas::find($id)
-        ]);
+    public function index($id)
+    {
+        $user = User::all();
+        Notification::send($user, new NotifContri($id->name));
+        return view('product', ["item" => Datas::find($id)]);
     }
 }
